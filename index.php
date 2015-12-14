8dad4b3f5eb838538e469d12f8ef97ee925fdb0f
lquispe <liliana.quispe@gmail.com>
2015-12-14 07:02:30 +0000

    cambios

---
+++ index.php
@@ -0,0 +1,172 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Relocation</title>
+
+    <!-- Bootstrap Core CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom CSS -->
+    <link href="css/modern-business.css" rel="stylesheet">
+
+    <!-- Custom Fonts -->
+    <link type="text/css" rel="stylesheet" href="http://fonts.googleapis.com/css?family=Montserrat">
+   <link type="text/css" rel="stylesheet" href="http://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,700,300,600,400">
+    <link href="font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
+</head>
+
+<body>
+
+    <!-- Navigation -->
+       <?php include 'header.html'; ?>
+
+
+    <!-- Header Carousel -->
+
+    
+    
+    <!-- Page Content -->
+    <div class="container" style="position:relative">
+            <div class="col-md-8 contppal font-primary">
+          <h3>Relocation service</h3></h3>
+          <h4>Por el mundo</h4>
+          <p>l traslado a otro país significa mucho más que un cambio de trabajo. Involucra un proceso complejo, el cual abarca desde el momento en que la empresa decide la designación del nuevo cargo, hasta que el expatriado y su familia se encuentran instalados y adaptados al nuevo medio.
+Muchas veces las empresas informan a sus empleados sobre los usos y costumbres ligadas a la actividad profesional, pero quien se ocupa de todo el resto?
+En efecto, una adaptación al lugar no es completa hasta que el expatriado y su familia estén instalados confortablemente en su nuevo hogar.
+BARC ayuda a las familias ocupándose de todos los detalles prácticos y burocráticos. Trabajamos en estrecha colaboración con la dirección de Recursos Humanos de distintas empresas. La búsqueda de vivienda se agiliza debido a que mantenemos un contacto permanente con las agencias inmobiliarias quienes nos envían diariamente un listado de propiedades actualizadas.
+BARC propone un plan flexible de servicios, para adaptarse mas fácilmente a las distintas exigencias de sus clientes.
+BARC se comunica directamente con el interesado y su familia disminuyendo las tensiones y la angustia que provocan los traslados hacia un nuevo destino.
+Los servicios de BARC están pensados para asegurar que la ‘relocación’ sea una experiencia positiva para el expatriado y su familia, facilitando el cambio, tanto de los que llegan a la Argentina, como de los que parten hacia otro destino.</p>
+          <a class="btn btn-info" href="portfolio-item.html">View Project</a>
+        </div>
+        <div class="col-md-4 contppal">
+          <div id="contenedor">
+    <div id="myCarousel" class="carousel slide">
+      <ol class="carousel-indicators">
+        <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+        <li data-target="#myCarousel" data-slide-to="1"></li>
+        <li data-target="#myCarousel" data-slide-to="2"></li>
+        <li data-target="#myCarousel" data-slide-to="3"></li>
+        <li data-target="#myCarousel" data-slide-to="4"></li>
+      </ol>
+      <!-- Carousel items -->
+      <div class="carousel-inner">
+        <div class="active item"><img  src="img/relocation.png" alt="banner1" /></div>
+        <div class="item"><img  src="img/relocation.png" alt="banner2" /></div>
+        <div class="item"><img  src="img/relocation.png" alt="banner3" /></div>
+        <div class="item"><img  src="img/relocation.png" alt="banner4" /></div>
+        <div class="item"><img  src="img/relocation.png" alt="banner5" /></div>
+      </div>
+      <!-- Carousel nav -->
+      <a class="carousel-control left" href="#myCarousel" data-slide="prev">&lsaquo;</a>
+      <a class="carousel-control right" href="#myCarousel" data-slide="next">&rsaquo;</a>
+    </div>
+</div>
+ 
+<script src="js/jquery.js"></script>
+<script src="js/bootstrap.min.js"></script>
+<script>
+    $(document).ready(function(){
+        $('.myCarousel').carousel()
+    });
+</script>
+        </div>
+
+        <!-- Marketing Icons Section -->
+        <div class="row">
+            <div class="col-lg-12">
+                <h1 class="page-header">
+                
+                </h1>
+            </div>
+            <div class="col-md-4">
+                <div class="panel ">
+                    <div >
+                        <h4><i class="fa fa-fw fa-check blackincolor"></i> Bootstrap v3.2.0</h4>
+                    </div>
+                    <div class="panel-body">
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Itaque, optio corporis quae nulla aspernatur in alias at numquam rerum ea excepturi expedita tenetur assumenda voluptatibus eveniet incidunt dicta nostrum quod?</p>
+                       
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="panel ">
+                    <div >
+                        <h4><i class="fa fa-fw fa-gift blackincolor"></i> Free &amp; Open Source</h4>
+                    </div>
+                    <div class="panel-body">
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Itaque, optio corporis quae nulla aspernatur in alias at numquam rerum ea excepturi expedita tenetur assumenda voluptatibus eveniet incidunt dicta nostrum quod?</p>
+                        
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="panel">
+                    <div>
+                        <h4><i class="fa fa-fw fa-compass blackincolor"></i> Easy to Use</h4>
+                    </div>
+                    <div class="panel-body">
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Itaque, optio corporis quae nulla aspernatur in alias at numquam rerum ea excepturi expedita tenetur assumenda voluptatibus eveniet incidunt dicta nostrum quod?</p>
+                       
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- /.row -->
+
+        <!-- Portfolio Section -->
+        <hr>
+
+        <!-- Call to Action Section -->
+        <div class="well">
+            <div class="row">
+                <div class="col-md-8">
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Molestias, expedita, saepe, vero rerum deleniti beatae veniam harum neque nemo praesentium cum alias asperiores commodi.</p>
+                </div>
+                <div class="col-md-4">
+                    <a class="btn btn-lg btn-default btn-block" href="#">Call to Action</a>
+                </div>
+            </div>
+        </div>
+
+        <hr>
+
+        <!-- Footer -->
+    
+        <?php include 'footer.html'; ?>
+    </div>
+    <!-- /.container -->
+
+    <!-- jQuery -->
+    <script src="js/jquery.js"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="js/bootstrap.min.js"></script>
+
+    <!-- Script to Activate the Carousel -->
+    <script>
+    $('.carousel').carousel({
+        interval: 5000 //changes the speed
+    })
+    </script>
+
+</body>
+
+</html>
